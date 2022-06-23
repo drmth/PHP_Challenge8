0ef802265f7fb36591e0fb0e1ae28fd3d333bcb5
drmth <79118141+drmth@users.noreply.github.com>
2022-06-23 12:49:24 +0200

    form secured

--- form.php
+++ form.php
@@ -1,29 +1,29 @@
 <form action = "/thanks.php" method="post">
     <div>
         <label for="lastname">Nom :</label>
-        <input type="text" id="last_name" name="user_last_name" required autofocus>
+        <input type="text" id="last_name" name="user_last_name" autofocus>
     </div>
     <div>
         <label for="first_name">Prénom :</label>
-        <input type="text" id="first_name" name="user_first_name" required>
+        <input type="text" id="first_name" name="user_first_name" >
     </div>
     <div>
         <label for="email">@ :</label>
-        <input type="email" id="email" name="user_email" required>
+        <input type="email" id="email" name="user_email" >
     </div>
     <div>
         <label for="tel">Tel :</label>
-        <input type="tel" id="tel" name="user_tel" required>
+        <input type="tel" id="tel" name="user_tel" >
     </div>
     <label for="subject">Sujet: </label>
-    <SELECT name="subject" id="subject" size="1">
-        <OPTION value="need drugs">Need drugs</OPTION>
-        <OPTION value="want to win Euromillion">Win Euromillion</OPTION>
-        <OPTION value="need to sleep">Need to sleep</OPTION>
-    </SELECT>
+    <select name="subject" id="subject" size="1">
+        <option value="need drugs">Need drugs</option>
+        <option value="want to win Euromillion">Win Euromillion</option>
+        <option value="need to sleep">Need to sleep</option>
+    </select>
     <div>
         <label for="message">Message</label>
-        <textarea name="message" id="message" required></textarea>
+        <textarea name="message" id="message" ></textarea>
     </div>
     <div class="button">
         <button type="submit">Envoyer votre message</button>
